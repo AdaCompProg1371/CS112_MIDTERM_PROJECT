9ff1c41b2cb029e73c26aaabadb6ecbbf54041e8
AdaCompProg1371 <144907186+AdaCompProg1371@users.noreply.github.com>
2023-11-07 23:28:29 +0800

    Update MagisaAndAmpuan_CS112_MIDTERM PROJECT.py

--- MagisaAndAmpuan_CS112_MIDTERM PROJECT.py
+++ MagisaAndAmpuan_CS112_MIDTERM PROJECT.py
@@ -1,8 +1,9 @@
 # Calculation of 7 Grocery Prices
-print("Beginning of Program")
-print("**************************")
+
+print("Welcome to Trailblazers Grocery Store! Today, we are having a monthly discounted price promo which happens on the first Wednesday of each month for seven products. Please input the required information needed.")
 
 # Type products
+print("Seven Products")
 product1 = input("Product 1:")
 product2 = input("Product 2:")
 product3 = input("Product 3:")
@@ -13,6 +14,7 @@ product7 = input("Product 7:")
 print("**************************")
 
 # Type prices for each product in pesos
+print("Prices for Each Product in Pesos")
 price1 = eval(input("Price 1:"))
 price2 = eval(input("Price 2:"))
 price3 = eval(input("Price 3:"))
@@ -22,7 +24,8 @@ price6 = eval(input("Price 6:"))
 price7 = eval(input("Price 7:"))
 print("**************************")
 
-# Type number of products for each product provided
+# Type the number of products for each product provided
+print("Quantity for Each Product")
 quantity1 = int(input("Quantity 1:"))
 quantity2 = int(input("Quantity 2:"))
 quantity3 = int(input("Quantity 3:"))
@@ -32,7 +35,8 @@ quantity6 = int(input("Quantity 6:"))
 quantity7 = int(input("Quantity 7:"))
 print("**************************")
 
-# Get valued price or VP for each product in pesos
+# Get the valued price or VP for each product in pesos
+print("Value Price or VP for Each Product")
 valued_price1 = price1 * quantity1
 print("VP1:" , valued_price1)
 valued_price2 = price2 * quantity2
@@ -49,12 +53,15 @@ valued_price7 = price7 * quantity7
 print("VP7:" , valued_price7)
 print("**************************")
 
-# Compute total price of all seven products in  pesos
+# Compute the total price of all seven products in  pesos
+print("Total Price for All Products")
 total_price = valued_price1 + valued_price2 + valued_price3 + valued_price4 + valued_price5 + valued_price6 + valued_price7
-print("Total Price:" , total_price)
+total_price1 = "{:.2f}".format(total_price)
+print("Total Price:" , total_price , "pesos")
 print("**************************")
 
 # Insert consumer's age for discount
+print("Discounted Price Based on Consumer's Age")
 consumer_age = eval(input("Consumer's Age:"))
 if consumer_age < 25:
     discount = "100 pesos"
@@ -66,10 +73,8 @@ print("Discount:" , discount)
 print("**************************")
 
 # Solve for discounted price in pesos
-discounted_price = total_price - consumer_age
+print("Discounted Price (in Pesos) as Official Payment for This Month's Promo")
+discounted_price = total_price - discounted_by_age
 discounted_price1 = "{:.2f}".format(discounted_price)
-print("Discounted Price:" , discounted_price1)
-print("**************************")
-
-# End of program
-print("End of Program")
+print("Discounted Price:" , discounted_price1 , "pesos")
+print("Congratulations! Thank you for shopping at Trailblazers Grocery Store and come again next time for the next month's promo.")
